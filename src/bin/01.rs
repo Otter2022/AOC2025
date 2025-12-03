dca12990169170395e25d0410b7e5796a177c16c
Otter2022 <ottoz@live.com>
2025-12-03 11:23:38 -0600

    day 1 complete

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -1,11 +1,40 @@
 advent_of_code::solution!(1);
 
-pub fn part_one(input: &str) -> Option<u64> {
-    None
+pub fn part_one(input: &str) -> Option<i64> {
+    let mut dial: i64 = 50;
+    let mut pswrd: i64 = 0;
+    for line in input.lines() {
+        let mut chars = line.chars();
+        let direction = chars.next().unwrap();
+        let mut clicks: i64 = chars.as_str().parse().unwrap();
+        let full_rotations = clicks.div_euclid(100);
+        pswrd = pswrd + full_rotations;
+        clicks = clicks - (100 * full_rotations);
+        
+        if direction == 'L' {
+            let temp_dial = dial - clicks;
+            if temp_dial < 0 && dial != 0 {pswrd += 1}
+            dial = (((dial - clicks) % 100) + 100) % 100;
+            // println!("{temp_dial}", temp_dial=temp_dial)
+        } else {
+            let temp_dial = dial + clicks;
+            if temp_dial > 100 && dial != 0 {pswrd += 1}
+            dial = (((dial + clicks) % 100) + 100) % 100;
+            // println!("{temp_dial}", temp_dial=temp_dial)
+        }
+
+        if dial == 0 {
+            pswrd = pswrd + 1;
+        }
+
+        println!("{dial} | {pswrd}", dial=dial, pswrd=pswrd)
+    }
+    println!("password: {pswrd}", pswrd=pswrd);
+    Some(pswrd)
 }
 
-pub fn part_two(input: &str) -> Option<u64> {
-    None
+pub fn part_two(input: &str) -> Option<i64> {
+    Some(44)
 }
 
 #[cfg(test)]
