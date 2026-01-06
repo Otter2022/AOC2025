3252133118eab1a0504b6e8346f5a23ffef62fb0
Otter2022 <ottoz@live.com>
2026-01-05 18:28:32 -0600

    day4 complete

--- src/bin/04.rs
+++ src/bin/04.rs
@@ -7,60 +7,114 @@ const DIRS_8: [(isize, isize); 8] = [
 ];
 
 pub fn make_grid(input: &str) -> Vec<Vec<char>> {
-    let height: usize = input.lines().count();
-    let width: usize = input.lines().next().unwrap().len();
+    let height = input.lines().count();
+    let width  = input.lines().next().unwrap().len();
+
     let mut grid: Vec<Vec<char>> = vec![vec![' '; width]; height];
-    
-    let mut x = 0;
-    let mut y = 0;
+
+    let mut row = 0;
     for line in input.lines() {
+        let mut col = 0;
         for c in line.chars() {
-            grid[y][x] = c;
-            x = x + 1;
+            grid[row][col] = c;
+            col += 1;
         }
-        y = y + 1;
+        row += 1;
     }
 
     grid
 }
 
+pub fn print_grid(grid: &Vec<Vec<char>>) {
+    for row in grid {
+        for c in row {
+            print!("{c}");
+        }
+        println!();
+    }
+}
+
 pub fn part_one(input: &str) -> Option<u64> {
+    let height = input.lines().count();
+    let width  = input.lines().next()?.len();
+    let grid   = make_grid(input);
+
     let mut roll_count: u64 = 0;
-    let height: usize = input.lines().count();
-    let width: usize = input.lines().next().unwrap().len();
-    let grid= make_grid(input);
 
-    for x in 0..(height) {
-        for y in 0..(width) {
-            if grid[x][y] == '@' {
-
+    for row in 0..height {
+        for col in 0..width {
+            if grid[row][col] == '@' {
                 let mut num_rolls = 0;
-                for (dx, dy) in DIRS_8 {
-                    let nx = x as isize + dx;
-                    let ny = y as isize + dy;
-                
-                    if nx < 0 || ny < 0 { continue; }
-                    let (nx, ny) = (nx as usize, ny as usize);
-                    if nx >= height || ny >= width { continue; }
-                
-                    let val = grid[nx][ny];
 
-                    if val == '@' {
+                for (dr, dc) in DIRS_8 {
+                    let nr = row as isize + dr;
+                    let nc = col as isize + dc;
+
+                    if nr < 0 || nc < 0 { continue; }
+                    let (nr, nc) = (nr as usize, nc as usize);
+                    if nr >= height || nc >= width { continue; }
+
+                    if grid[nr][nc] == '@' {
                         num_rolls += 1;
                     }
                 }
+
                 if num_rolls < 4 {
                     roll_count += 1;
                 }
             }
         }
     }
-    println!("########### {} ###########", roll_count);
+
+    println!(" ########### {} ############## ", roll_count);
     Some(roll_count)
 }
 
+pub fn recurse_to_get_rolls(grid: &mut Vec<Vec<char>>, roll_count: u64, height: usize, width: usize) -> Option<u64> {
+    
+
+    let mut new_roll_count: u64 = roll_count;
+
+    for row in 0..height {
+        for col in 0..width {
+            if grid[row][col] == '@' {
+                let mut num_rolls = 0;
+
+                for (dr, dc) in DIRS_8 {
+                    let nr = row as isize + dr;
+                    let nc = col as isize + dc;
+
+                    if nr < 0 || nc < 0 { continue; }
+                    let (nr, nc) = (nr as usize, nc as usize);
+                    if nr >= height || nc >= width { continue; }
+
+                    if grid[nr][nc] == '@' {
+                        num_rolls += 1;
+                    }
+                }
+
+                if num_rolls < 4 {
+                    new_roll_count += 1;
+                    grid[row][col] = '.';
+                }
+            }
+        }
+    }
+
+    println!("# {} #", new_roll_count);
+    print_grid(grid);
+    if new_roll_count != roll_count {
+        recurse_to_get_rolls(grid, new_roll_count, height, width)
+    } else {
+        Some(new_roll_count)
+    }
+}
+
 pub fn part_two(input: &str) -> Option<u64> {
-    None
+    let height = input.lines().count();
+    let width  = input.lines().next()?.len();
+    let mut grid   = make_grid(input);
+    recurse_to_get_rolls(&mut grid, 0, height, width)
 }
 
 #[cfg(test)]
